9318f3135c8549bc0300ab38b040d14113ec70af
jariar01 <jariar01@estudiantes.unileon.es>
2019-02-20 11:17:23 +0100

    Detecta_tres

---
+++ Detecta_tres.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+int main()
+{int Num;
+printf("Escribe un numero: ");
+scanf("%d", &Num);
+if (Num == 3)
+{printf("El numero introducido es el 3\n");}
+else
+{printf("El numero introducido no es el 3\n");}
+return 0;
+}
